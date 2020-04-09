9046afa89fa01cba5f30c0e08237e8c267cc036d
jonathanhhoward <2773262@gmail.com>
2020-04-08 20:31:53 -0400

    Implement 'Test POST /api/books with no title given'

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -36,6 +36,8 @@ suite('Functional Tests', function () {
   */
 
   suite('Routing tests', function () {
+    const basicRoute = '/api/books'
+    const idRoute = '/api/books/:id'
 
     suite('POST /api/books with title => create book object/expect book object', function () {
 
@@ -45,7 +47,7 @@ suite('Functional Tests', function () {
         }
 
         chai.request(server)
-          .post('/api/books')
+          .post(basicRoute)
           .send(book)
           .end(function (err, res) {
             assert.strictEqual(res.status, 200)
@@ -54,8 +56,19 @@ suite('Functional Tests', function () {
           })
       })
 
-      test.skip('Test POST /api/books with no title given', function (done) {
-        //done();
+      test('Test POST /api/books with no title given', function (done) {
+        const book = {
+          title: ''
+        }
+
+        chai.request(server)
+          .post(basicRoute)
+          .send(book)
+          .end(function (err, res) {
+            assert.strictEqual(res.status, 200)
+            assert.strictEqual(res.text, 'missing title')
+            done()
+          })
       })
 
     })
