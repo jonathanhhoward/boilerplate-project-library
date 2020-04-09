bc877de8796df096b211d4c7084fc7590f7ef88e
jonathanhhoward <2773262@gmail.com>
2020-04-09 13:44:12 -0400

    Remove example

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -15,25 +15,6 @@ chai.use(chaiHttp)
 
 suite('Functional Tests', function () {
 
-  /*
-  * ----[EXAMPLE TEST]----
-  * Each test should completely test the response of the API end-point including response status code!
-  */
-  test.skip('#example Test GET /api/books', function (done) {
-    chai.request(server)
-      .get('/api/books')
-      .end(function (err, res) {
-        assert.equal(res.status, 200)
-        assert.isArray(res.body, 'response should be an array')
-        assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount')
-        assert.property(res.body[0], 'title', 'Books in array should contain title')
-        assert.property(res.body[0], '_id', 'Books in array should contain _id')
-        done()
-      })
-  })
-  /*
-  * ----[END of EXAMPLE TEST]----
-  */
 
   suite('Routing tests', function () {
     const basicRoute = '/api/books'
