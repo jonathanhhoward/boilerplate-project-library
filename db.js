6ccaf0e79467f404fe5a8f7134627be53bef1f7d
jonathanhhoward <2773262@gmail.com>
2020-04-15 16:50:02 -0400

    Move to model.js

--- db.js
+++
@@ -1,7 +0,0 @@
-const { MongoClient } = require('mongodb')
-
-const mongoClient = new MongoClient(process.env.MONGODB_URI, { useUnifiedTopology: true })
-
-module.exports = {
-  connect: mongoClient.connect()
-}
