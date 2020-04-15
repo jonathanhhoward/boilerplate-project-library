570c2cb5c692ea0bf0fae67d8d47982354bb5f5f
jonathanhhoward <2773262@gmail.com>
2020-04-15 17:13:44 -0400

    Reformat

--- model.js
+++ model.js
@@ -9,83 +9,76 @@ const connection = new MongoClient(
 ).connect()
 
 module.exports = {
-  getAll,
-  create,
-  drop,
-  get,
-  update,
-  remove
-}
+  getAll: async function () {
+    try {
+      const client = await connection
+      const results = await client.db().collection('books')
+        .find()
+      return results.toArray()
+    } catch (e) {
+      console.error(e)
+    }
+  },
 
-async function getAll () {
-  try {
-    const client = await connection
-    const results = await client.db().collection('books')
-      .find()
-    return results.toArray()
-  } catch (e) {
-    console.error(e)
-  }
-}
+  create: async function (title) {
+    const book = {
+      title: title,
+      comments: []
+    }
 
-async function create (title) {
-  const book = {
-    title: title,
-    comments: []
-  }
+    try {
+      const client = await connection
+      const result = await client.db().collection('books')
+        .insertOne(book)
+      return result.ops[0]
+    } catch (err) {
+      console.error(err)
+    }
+  },
 
-  try {
-    const client = await connection
-    const result = await client.db().collection('books')
-      .insertOne(book)
-    return result.ops[0]
-  } catch (err) {
-    console.error(err)
-  }
-}
+  drop: async function () {
+    try {
+      const client = await connection
+      return await client.db().collection('books')
+        .drop()
+    } catch (err) {
+      console.error(err)
+    }
+  },
 
-async function drop () {
-  try {
-    const client = await connection
-    return await client.db().collection('books')
-      .drop()
-  } catch (err) {
-    console.error(err)
-  }
-}
+  get: async function (id) {
+    try {
+      const client = await connection
+      return await client.db().collection('books')
+        .findOne({ _id: ObjectID(id) })
+    } catch (err) {
+      console.error(err)
+    }
+  },
 
-async function get (id) {
-  try {
-    const client = await connection
-    return await client.db().collection('books')
-      .findOne({ _id: ObjectID(id) })
-  } catch (err) {
-    console.error(err)
-  }
-}
+  update: async function (id, comment) {
+    try {
+      const client = await connection
+      const result = await client.db().collection('books')
+        .findOneAndUpdate(
+          { _id: ObjectID(id) },
+          { $push: { comments: comment } },
+          { returnOriginal: false }
+        )
+      return result.value
+    } catch (err) {
+      console.error(err)
+    }
+  },
 
-async function update (id, comment) {
-  try {
-    const client = await connection
-    const result = await client.db().collection('books')
-      .findOneAndUpdate(
-        { _id: ObjectID(id) },
-        { $push: { comments: comment } },
-        { returnOriginal: false }
-      )
-    return result.value
-  } catch (err) {
-    console.error(err)
-  }
-}
-
-async function remove (id) {
-  try {
-    const client = await connection
-    const result = await client.db().collection('books')
-      .findOneAndDelete({ _id: ObjectID(id) })
-    return result.value
-  } catch (err) {
-    console.error(err)
+  remove: async function (id) {
+    try {
+      const client = await connection
+      const result = await client.db().collection('books')
+        .findOneAndDelete({ _id: ObjectID(id) })
+      return result.value
+    } catch (err) {
+      console.error(err)
+    }
   }
 }
