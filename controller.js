012771b4f84c1d3f14b41b3670c2f17224bf71b2
jonathanhhoward <2773262@gmail.com>
2020-04-15 17:17:14 -0400

    Reformat

--- controller.js
+++ controller.js
@@ -3,93 +3,86 @@
 const Book = require('./model')
 
 module.exports = {
-  listBooks,
-  clearBooks,
-  addBook,
-  removeBook,
-  addComment,
-  viewBook
-}
+  listBooks: async function (req, res, next) {
+    try {
+      const books = await Book.getAll()
 
-async function listBooks (req, res, next) {
-  try {
-    const books = await Book.getAll()
-
-    res.json(
-      books.map((book) => ({
-        _id: book._id,
-        title: book.title,
-        commentcount: book.comments.length
-      }))
-    )
-  } catch (err) {
-    next(err)
-  }
-}
-
-async function addBook (req, res, next) {
-  if (!req.body.title) return res.send('missing title')
-
-  try {
-    const book = await Book.create(req.body.title)
-    res.json(book)
-  } catch (err) {
-    next(err)
-  }
-}
-
-async function clearBooks (req, res, next) {
-  try {
-    const success = await Book.drop()
-
-    if (success) {
-      res.send('complete delete successful')
-    } else {
-      res.send('no deletion')
+      res.json(
+        books.map((book) => ({
+          _id: book._id,
+          title: book.title,
+          commentcount: book.comments.length
+        }))
+      )
+    } catch (err) {
+      next(err)
     }
-  } catch (err) {
-    next(err)
-  }
-}
+  },
 
-async function viewBook (req, res, next) {
-  try {
-    const book = await Book.get(req.params.id)
+  addBook: async function (req, res, next) {
+    if (!req.body.title) return res.send('missing title')
 
-    if (book) {
+    try {
+      const book = await Book.create(req.body.title)
       res.json(book)
-    } else {
-      res.send('no book exists')
+    } catch (err) {
+      next(err)
     }
-  } catch (err) {
-    next(err)
-  }
-}
+  },
 
-async function addComment (req, res, next) {
-  try {
-    const book = await Book.update(req.params.id, req.body.comment)
+  clearBooks: async function (req, res, next) {
+    try {
+      const success = await Book.drop()
 
-    if (book) {
-      res.json(book)
-    } else {
-      res.send('no book exists')
+      if (success) {
+        res.send('complete delete successful')
+      } else {
+        res.send('no deletion')
+      }
+    } catch (err) {
+      next(err)
     }
-  } catch (err) {
-    next(err)
-  }
-}
+  },
 
-async function removeBook (req, res, next) {
-  try {
-    const success = await Book.remove(req.params.id)
+  viewBook: async function (req, res, next) {
+    try {
+      const book = await Book.get(req.params.id)
 
-    if (success) {
-      res.send('delete successful')
-    } else {
-      res.send('no book exists')
+      if (book) {
+        res.json(book)
+      } else {
+        res.send('no book exists')
+      }
+    } catch (err) {
+      next(err)
     }
-  } catch (err) {
-    next(err)
+  },
+
+  addComment: async function (req, res, next) {
+    try {
+      const book = await Book.update(req.params.id, req.body.comment)
+
+      if (book) {
+        res.json(book)
+      } else {
+        res.send('no book exists')
+      }
+    } catch (err) {
+      next(err)
+    }
+  },
+
+  removeBook: async function (req, res, next) {
+    try {
+      const success = await Book.remove(req.params.id)
+
+      if (success) {
+        res.send('delete successful')
+      } else {
+        res.send('no book exists')
+      }
+    } catch (err) {
+      next(err)
+    }
   }
 }
