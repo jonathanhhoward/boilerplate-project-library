fb2d98d3afe05db0937be7d057a15dd31e964423
jonathanhhoward <2773262@gmail.com>
2020-04-16 12:28:02 -0400

    Extract try/catch blocks to autoCatch()

--- controller.js
+++ controller.js
@@ -1,88 +1,65 @@
 'use strict'
 
 const Book = require('./model')
+const autoCatch = require('./auto-catch')
 
-module.exports = {
-  listBooks: async function (req, res, next) {
-    try {
-      const books = await Book.getAll()
+module.exports = autoCatch({
+  listBooks: async function (req, res) {
+    const books = await Book.list()
 
-      res.json(
-        books.map((book) => ({
-          _id: book._id,
-          title: book.title,
-          commentcount: book.comments.length
-        }))
-      )
-    } catch (err) {
-      next(err)
-    }
+    res.json(
+      books.map((book) => ({
+        _id: book._id,
+        title: book.title,
+        commentcount: book.comments.length
+      }))
+    )
   },
 
-  addBook: async function (req, res, next) {
+  createBook: async function (req, res) {
     if (!req.body.title) return res.send('missing title')
 
-    try {
-      const book = await Book.create(req.body.title)
+    const book = await Book.create(req.body.title)
+    res.json(book)
+  },
+
+  clearBooks: async function (req, res) {
+    const success = await Book.drop()
+
+    if (success) {
+      res.send('complete delete successful')
+    } else {
+      res.send('no deletion')
+    }
+  },
+
+  getBook: async function (req, res) {
+    const book = await Book.get(req.params.id)
+
+    if (book) {
       res.json(book)
-    } catch (err) {
-      next(err)
+    } else {
+      res.send('no book exists')
     }
   },
 
-  clearBooks: async function (req, res, next) {
-    try {
-      const success = await Book.drop()
+  addComment: async function (req, res) {
+    const book = await Book.update(req.params.id, req.body.comment)
 
-      if (success) {
-        res.send('complete delete successful')
-      } else {
-        res.send('no deletion')
-      }
-    } catch (err) {
-      next(err)
+    if (book) {
+      res.json(book)
+    } else {
+      res.send('no book exists')
     }
   },
 
-  viewBook: async function (req, res, next) {
-    try {
-      const book = await Book.get(req.params.id)
+  removeBook: async function (req, res) {
+    const success = await Book.remove(req.params.id)
 
-      if (book) {
-        res.json(book)
-      } else {
-        res.send('no book exists')
-      }
-    } catch (err) {
-      next(err)
-    }
-  },
-
-  addComment: async function (req, res, next) {
-    try {
-      const book = await Book.update(req.params.id, req.body.comment)
-
-      if (book) {
-        res.json(book)
-      } else {
-        res.send('no book exists')
-      }
-    } catch (err) {
-      next(err)
-    }
-  },
-
-  removeBook: async function (req, res, next) {
-    try {
-      const success = await Book.remove(req.params.id)
-
-      if (success) {
-        res.send('delete successful')
-      } else {
-        res.send('no book exists')
-      }
-    } catch (err) {
-      next(err)
+    if (success) {
+      res.send('delete successful')
+    } else {
+      res.send('no book exists')
     }
   }
-}
+})
--- model.js
+++ model.js
@@ -8,15 +8,13 @@ const connection = new MongoClient(
 ).connect()
 
 module.exports = {
-  getAll: async function () {
-    try {
-      const client = await connection
-      const results = await client.db().collection('books')
-        .find()
-      return results.toArray()
-    } catch (e) {
-      console.error(e)
-    }
+  list: async function () {
+    const client = await connection
+
+    const results = await client.db().collection('books')
+      .find()
+
+    return results.toArray()
   },
 
   create: async function (title) {
@@ -25,59 +23,47 @@ module.exports = {
       comments: []
     }
 
-    try {
-      const client = await connection
-      const result = await client.db().collection('books')
-        .insertOne(book)
-      return result.ops[0]
-    } catch (err) {
-      console.error(err)
-    }
+    const client = await connection
+
+    const result = await client.db().collection('books')
+      .insertOne(book)
+
+    return result.ops[0]
   },
 
   drop: async function () {
-    try {
-      const client = await connection
-      return await client.db().collection('books')
-        .drop()
-    } catch (err) {
-      console.error(err)
-    }
+    const client = await connection
+
+    return await client.db().collection('books')
+      .drop()
   },
 
   get: async function (id) {
-    try {
-      const client = await connection
-      return await client.db().collection('books')
-        .findOne({ _id: ObjectID(id) })
-    } catch (err) {
-      console.error(err)
-    }
+    const client = await connection
+
+    return await client.db().collection('books')
+      .findOne({ _id: ObjectID(id) })
   },
 
   update: async function (id, comment) {
-    try {
-      const client = await connection
-      const result = await client.db().collection('books')
-        .findOneAndUpdate(
-          { _id: ObjectID(id) },
-          { $push: { comments: comment } },
-          { returnOriginal: false }
-        )
-      return result.value
-    } catch (err) {
-      console.error(err)
-    }
+    const client = await connection
+
+    const result = await client.db().collection('books')
+      .findOneAndUpdate(
+        { _id: ObjectID(id) },
+        { $push: { comments: comment } },
+        { returnOriginal: false }
+      )
+
+    return result.value
   },
 
   remove: async function (id) {
-    try {
-      const client = await connection
-      const result = await client.db().collection('books')
-        .findOneAndDelete({ _id: ObjectID(id) })
-      return result.value
-    } catch (err) {
-      console.error(err)
-    }
+    const client = await connection
+
+    const result = await client.db().collection('books')
+      .findOneAndDelete({ _id: ObjectID(id) })
+
+    return result.value
   }
 }
