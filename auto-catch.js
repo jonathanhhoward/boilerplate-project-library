a2d0b9b0a3b241e66f59bcdf8e1ddb24ab698abf
jonathanhhoward <2773262@gmail.com>
2020-04-16 13:33:25 -0400

    Remove function name

--- auto-catch.js
+++ auto-catch.js
@@ -1,4 +1,4 @@
-module.exports = function autoCatch (handlers) {
+module.exports = function (handlers) {
   return Object.keys(handlers).reduce((autoHandlers, key) => {
     const handler = handlers[key]
     autoHandlers[key] = (req, res, next) =>
